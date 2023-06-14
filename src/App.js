19b42ea49bd729dda71f05268723bea7afba11fc
Utku Cengiz <cekicengiz01@gmail.com>
2023-06-14 19:30:58 +0300

    navbar laptop

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import { GiHamburgerMenu } from "react-icons/gi";
 
 function App() {
   return (
-    <div className="flex mx-6 my-2 justify-between items-center">
-      <h1 className="uppercase text-custom-purple font-black text-2xl	">
+    <div className="flex mx-6 my-2 justify-between items-center lg:mx-12 lg:my-4">
+      <h1 className="uppercase text-custom-purple font-black text-2xl	lg:text-2xl">
         NFters
       </h1>
       <div className="hidden md:block">
-        <a href="" className="px-2 text-black font-dmSans font-medium text-sm">
+        <a
+          href=""
+          className="px-2 text-black font-dmSans font-medium text-sm lg:text-lg"
+        >
           Marketplace
         </a>
-        <a href="" className="px-2 text-black font-dmSans font-medium text-sm">
+        <a
+          href=""
+          className="px-2 text-black font-dmSans font-medium text-sm lg:text-lg"
+        >
           Resource
         </a>
-        <a href="" className="px-2 text-black font-dmSans font-medium text-sm">
+        <a
+          href=""
+          className="px-2 text-black font-dmSans font-medium text-sm lg:text-lg"
+        >
           About
         </a>
       </div>
@@ -22,15 +31,15 @@ function App() {
         <input
           type="text"
           placeholder="Search"
-          className="border border-gray-300 py-1 pl-2 rounded-2xl text-sm placeholder:text-gray-500 "
+          className="border border-2 border-gray-300 py-1 pl-2 lg:py-2 lg:pl-4 rounded-2xl lg:rounded-3xl text-sm placeholder:text-gray-500 placeholder:font-dmSans font-dmSans lg:text-lg "
         />
-        <CiSearch className="absolute right-2 text-gray-600" />
+        <CiSearch className="absolute right-2 text-gray-600 lg:text-2xl" />
       </div>
       <div className="hidden md:flex">
-        <span className="font-dmSans font-bold text-xs border border-transparent py-1 px-2 rounded-2xl bg-custom-purple text-white mr-1">
+        <span className="font-dmSans font-bold text-xs border border-2 border-transparent py-1 px-2 rounded-2xl bg-custom-purple text-white mr-1 lg:text-lg lg:rounded-3xl lg:py-2 lg:px-4">
           Upload
         </span>
-        <span className="font-dmSans font-bold text-xs border border-transparent py-1 px-2 rounded-2xl bg-custom-purple text-white ml-1">
+        <span className="font-dmSans font-bold text-xs border border-2 border-custom-purple py-1 px-2 rounded-2xl bg-white text-custom-purple ml-1 lg:text-lg lg:rounded-3xl lg:py-2 lg:px-4">
           Connect Wallet
         </span>
       </div>
