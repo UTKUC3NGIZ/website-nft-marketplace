50b2be00ac807539c1f5119b2b858efb84cb1735
Utku Cengiz <cekicengiz01@gmail.com>
2023-06-14 19:11:24 +0300

    navbar tablet

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import { GiHamburgerMenu } from "react-icons/gi";
 
 function App() {
   return (
-    <div className="flex mx-6 my-2 justify-between">
+    <div className="flex mx-6 my-2 justify-between items-center">
       <h1 className="uppercase text-custom-purple font-black text-2xl	">
         NFters
       </h1>
       <div className="hidden md:block">
-        <a href="" className="px-2 text-black">
+        <a href="" className="px-2 text-black font-dmSans font-medium text-sm">
           Marketplace
         </a>
-        <a href="" className="px-2 text-black">
+        <a href="" className="px-2 text-black font-dmSans font-medium text-sm">
           Resource
         </a>
-        <a href="" className="px-2 text-black">
+        <a href="" className="px-2 text-black font-dmSans font-medium text-sm">
           About
         </a>
       </div>
@@ -26,9 +26,13 @@ function App() {
         />
         <CiSearch className="absolute right-2 text-gray-600" />
       </div>
-      <div className="hidden md:block">
-        <span className="px-2">Upload</span>
-        <span className="px-2">Connect Wallet</span>
+      <div className="hidden md:flex">
+        <span className="font-dmSans font-bold text-xs border border-transparent py-1 px-2 rounded-2xl bg-custom-purple text-white mr-1">
+          Upload
+        </span>
+        <span className="font-dmSans font-bold text-xs border border-transparent py-1 px-2 rounded-2xl bg-custom-purple text-white ml-1">
+          Connect Wallet
+        </span>
       </div>
       <div className="flex items-center text-custom-purple md:hidden">
         <GiHamburgerMenu />
