91cf7dc510cdf24d9ac4b5bb67fb8401cee0fe05
Utku Cengiz <cekicengiz01@gmail.com>
2023-06-14 17:45:29 +0300

    font added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <div><h1 className="text-red-50">deneme</h1></div>;
+  return (
+    <div>
+      <h1 className="text-red-500 font-dmSans">deneme</h1>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        dmSans: ["DM Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
