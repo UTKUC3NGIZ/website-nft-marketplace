8ff783075b5075e6d2f16487333dc81993c7292b
Utku Cengiz <cekicengiz01@gmail.com>
2023-06-14 19:50:07 +0300

    navbar completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      'sm': '640px',
+
+      'md': '768px',
+
+      'lg': '1024px',
+
+      'xl': '1280px',
+
+      '2xl': '1440px',
+    },
     extend: {
       fontFamily: {
         dmSans: ["DM Sans", "sans-serif"],
