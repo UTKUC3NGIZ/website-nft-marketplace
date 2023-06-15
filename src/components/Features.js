30df1fdbc1d578f2d3c27f09423422ce8d259ce9
Utku Cengiz <cekicengiz01@gmail.com>
2023-06-15 14:17:05 +0300

    feature section completed

--- src/components/Features.js
+++ src/components/Features.js
@@ -3,12 +3,12 @@ import FastTransaction from "../assets/images/card-tick-1 1.svg";
 
 function Features() {
   return (
-    <div className="bg-slate-50 flex flex-row 2xl:px-[120px]  lg:px-12  px-6 py-28">
-      <h2 className="text-3xl font-bhSans mr-16 uppercase">
+    <div className="bg-slate-50 flex md:flex-row flex-col 2xl:px-[120px]  lg:px-12  px-6 py-28">
+      <h2 className="text-3xl font-bhSans md:mr-16 mb-10 md:mb-0 uppercase text-center">
         The amazing NFT art of the world here
       </h2>
-      <div className=" mr-16">
-        <div className="flex">
+      <div className=" md:mr-16 mb-10 md:mb-0">
+        <div className="flex items-start flex-row w-3/4 m-auto md:m-0 md:w-full">
           <img src={FastTransaction} alt="" className="mr-4" />
           <div className="flex flex-col">
             <h3 className="text-xl font-dmSans font-bold pb-3">
@@ -22,7 +22,7 @@ function Features() {
         </div>
       </div>
       <div className="">
-        <div className="flex">
+        <div className="flex items-start flex-row w-3/4 m-auto md:m-0 md:w-full">
           <img src={FastTransaction} alt="" className="mr-4" />
           <div className="flex flex-col">
             <h3 className="text-xl font-dmSans font-bold pb-3">
