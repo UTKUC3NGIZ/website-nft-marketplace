a4aa12dbc8a2cce593e66a2c98e1e98db670be28
Utku Cengiz <cekicengiz01@gmail.com>
2023-06-16 18:02:02 +0300

    footer completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,15 +5,15 @@ import linkedin from "../assets/images/linkedin.svg";
 
 function Footer() {
   return (
-    <div className="2xl:px-[120px]  lg:px-12  px-6 py-28 items-center flex gap-12">
-      <div className="w-1/2">
+    <div className="2xl:px-[120px]  lg:px-12  px-6 py-28 flex xl:gap-12 lg:gap-8 gap-2 sm:items-start flex-col md:flex-row items-center">
+      <div className="flex flex-col sm:w-1/2 w-full mb-10 sm:mb-0 text-left items-start ">
         <h3 className="font-bhSans text-2xl pb-7">NFTERS</h3>
-        <p className="font-normal text-sm text-gray-600">
+        <p className="font-normal text-sm text-gray-600  text-left">
           The world’s first and largest digital marketplace for crypto
           collectibles and non-fungible tokens (NFTs). Buy, sell, and discover
           exclusive digital items.
         </p>
-        <span className="flex gap-4 pt-8">
+        <span className="flex gap-4 pt-8 m-auto xsm:m-0">
           <a href="">
             <img src={facebook} alt="" />
           </a>
@@ -25,8 +25,8 @@ function Footer() {
           </a>
         </span>
       </div>
-      <div className="flex justify-between">
-        <ul className="font-dmSans font-medium">
+      <div className="flex justify-between sm:flex-row flex-col items-start text-left">
+        <ul className="font-dmSans font-medium mb-10 sm:mb-0">
           <h3 className="font-bold pb-6">Market Place</h3>
           <li className="mb-2">
             <a href="">All NFTs</a>
@@ -50,7 +50,7 @@ function Footer() {
             <a href="">Domain Name</a>
           </li>
         </ul>
-        <ul className="font-dmSans font-medium">
+        <ul className="font-dmSans font-medium mb-10 sm:mb-0">
           <h3 className="font-bold pb-6">My Account</h3>
           <li className="mb-2">
             <a href="">Profile</a>
@@ -65,7 +65,7 @@ function Footer() {
             <a href="">Settings</a>
           </li>
         </ul>
-        <ul className="font-dmSans font-medium w-3/5">
+        <ul className="font-dmSans font-medium mb-10 sm:mb-0 sm:w-3/5 w-full">
           <h3 className="font-bold pb-6 ">Stay in the loop</h3>
           <p className="mb-6">
             Join our mailing list to stay in the loop with our newest feature
@@ -75,11 +75,11 @@ function Footer() {
             <input
               type="mail"
               placeholder="Enter your email address.."
-              className="border-2 w-full border-gray-100 py-5 px-6 rounded-full placeholder:text-gray-400 placeholder:font-dmSans placeholder:font-normal placeholder:text-xs "
+              className="border-2 w-full border-gray-100 py-5 px-6 rounded-full placeholder:text-gray-400 placeholder:font-dmSans placeholder:font-normal placeholder:text-xs lg:text-base  text-xs"
             />
             <button
               type="submit"
-              className="absolute border-2 bg-custom-purple text-white py-4 px-7 rounded-full right-2"
+              className="absolute border-2 bg-custom-purple text-white xl:py-4 sm:py-3 py-2 xl:px-7 sm:px-5 px-3 text-sm lg:text-lg rounded-full right-2"
             >
               Subscibe Now
             </button>
