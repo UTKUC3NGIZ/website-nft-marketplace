2b26a8ae4ef9309505a99e2e7e92b7cfa62eb479
Utku Cengiz <cekicengiz01@gmail.com>
2023-06-17 04:10:32 +0300

    navbar hover and focus

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,19 +10,19 @@ function Navbar() {
       <div className="hidden md:block">
         <a
           href=""
-          className="px-2 text-black font-dmSans font-medium text-sm lg:text-lg 2xl:mr-10"
+          className="px-2 text-black font-dmSans font-medium text-sm lg:text-lg 2xl:mr-10 hover:text-custom-purple"
         >
           Marketplace
         </a>
         <a
           href=""
-          className="px-2 text-black font-dmSans font-medium text-sm lg:text-lg 2xl:mr-10"
+          className="px-2 text-black font-dmSans font-medium text-sm lg:text-lg 2xl:mr-10 hover:text-custom-purple"
         >
           Resource
         </a>
         <a
           href=""
-          className="px-2 text-black font-dmSans font-medium text-sm lg:text-lg "
+          className="px-2 text-black font-dmSans font-medium text-sm lg:text-lg hover:text-custom-purple "
         >
           About
         </a>
@@ -31,15 +31,15 @@ function Navbar() {
         <input
           type="text"
           placeholder="Search"
-          className=" border-2 border-gray-300 py-1 pl-2 lg:py-2 lg:pl-4 rounded-2xl lg:rounded-3xl text-sm placeholder:text-gray-500 placeholder:font-dmSans font-dmSans lg:text-lg 2xl:w-[300px] "
+          className=" border-2 border-gray-300 py-1 pl-2 lg:py-2 lg:pl-4 rounded-2xl lg:rounded-3xl text-sm placeholder:text-gray-500 placeholder:font-dmSans font-dmSans lg:text-lg 2xl:w-[300px] outline-none hover:border-custom-purple focus:border-custom-purple"
         />
         <CiSearch className="absolute right-2 text-gray-600 lg:text-2xl" />
       </div>
       <div className="hidden md:flex">
-        <span className="font-dmSans font-bold text-xs  border-2 border-transparent py-1 px-2 rounded-2xl bg-custom-purple text-white mr-1 lg:text-lg lg:rounded-3xl lg:py-2 lg:px-4 2xl:px-8 2xl:mr-3">
+        <span className="font-dmSans font-bold text-xs  border-2 border-custom-purple py-1 px-2 rounded-2xl bg-custom-purple text-white mr-1 lg:text-lg lg:rounded-3xl lg:py-2 lg:px-4 2xl:px-8 2xl:mr-3 hover:bg-white hover:text-custom-purple">
           Upload
         </span>
-        <span className="font-dmSans font-bold text-xs  border-2 border-custom-purple py-1 px-2 rounded-2xl bg-white text-custom-purple ml-1 lg:text-lg lg:rounded-3xl lg:py-2 lg:px-4 2xl:px-8 2xl:ml-3">
+        <span className="font-dmSans font-bold text-xs  border-2 border-custom-purple py-1 px-2 rounded-2xl bg-white text-custom-purple ml-1 lg:text-lg lg:rounded-3xl lg:py-2 lg:px-4 2xl:px-8 2xl:ml-3 hover:bg-custom-purple hover:text-white">
           Connect Wallet
         </span>
       </div>
